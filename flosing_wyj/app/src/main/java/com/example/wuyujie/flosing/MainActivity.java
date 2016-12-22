d40c9607ee4416b33e7801c0ae13f13961de6a06
lostezra <lostezra@gmail.com>
2016-12-23 01:52:37 +0800

    登录界面，未完成版

---
+++ flosing_wyj/app/src/main/java/com/example/wuyujie/flosing/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.wuyujie.flosing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+        setContentView(R.layout.activity_main);
+    }
+}
