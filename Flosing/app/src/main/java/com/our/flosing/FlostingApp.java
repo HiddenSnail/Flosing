6e61dd0c1a4308d4909a3e3be415249c39ee8f34
runnishino <237485393@qq.com>
2016-12-26 00:24:30 +0800

    第一版Login后台逻辑，未与界面部分连接，未实现到register的转跳。

---
+++ Flosing/app/src/main/java/com/our/flosing/FlostingApp.java
@@ -0,0 +1,21 @@
+package com.our.flosing;
+
+import android.app.Application;
+
+import com.avos.avoscloud.AVAnalytics;
+import com.avos.avoscloud.AVOSCloud;
+
+/**
+ * Created by RunNishino on 2016/12/25.
+ */
+
+public class FlostingApp extends Application {
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+
+        AVOSCloud.initialize(this,"o0hgIj3eXndNi9oimoBf63sq-gzGzoHsz", "XaIIq0CsmKlY75CXPOUGISIW");
+
+    }
+}
--- Flosing/app/src/main/java/com/our/flosing/MainActivity.java
+++ Flosing/app/src/main/java/com/our/flosing/MainActivity.java
@@ -3,11 +3,28 @@ package com.our.flosing;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVObject;
+import com.avos.avoscloud.LogUtil;
+import com.avos.avoscloud.SaveCallback;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        //测试连接代码
+//        AVObject testObject = new AVObject("TestObject");
+//        testObject.put("words","Hello World!");
+//        testObject.saveInBackground(new SaveCallback() {
+//            @Override
+//            public void done(AVException e) {
+//                if (e == null){
+//                    LogUtil.log.d("save","success");
+//                }
+//            }
+//        });
     }
 }
