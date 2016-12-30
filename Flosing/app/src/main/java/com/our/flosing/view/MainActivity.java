57c16b0de31c12987b42383104c8d42f7dad8004
duozaishulindewoniu <741409033@qq.com>
2016-12-31 01:08:31 +0800

    Revert "添加下拉刷新功能，刷新不回弹bug未修复"
    This reverts commit 4db4347667b1f01a38d18c47dca308dbc74c45c4.

--- Flosing/app/src/main/java/com/our/flosing/view/MainActivity.java
+++ Flosing/app/src/main/java/com/our/flosing/view/MainActivity.java
@@ -10,15 +10,12 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.avos.avoscloud.AVUser;
-import com.handmark.pulltorefresh.library.PullToRefreshBase;
 import com.handmark.pulltorefresh.library.PullToRefreshListView;
 import com.our.flosing.R;
 import com.our.flosing.bean.LostCard;
 import com.our.flosing.bean.LostCardAdapter;
 import com.our.flosing.presenter.HomePagePresenter;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -27,44 +24,17 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements IHomePageView {
     static private HomePagePresenter homePagePresenter;
-    static private int pageNumber;
-    private PullToRefreshListView listView;
-    static private List<LostCard> mLostCards = new ArrayList<>();
-    LostCardAdapter lostCardAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = (PullToRefreshListView) findViewById(R.id.listview_lostcards);
-        listView.setMode(PullToRefreshBase.Mode.BOTH);
-        pageNumber = 1;
-
-
-
         if (homePagePresenter == null) homePagePresenter = new HomePagePresenter(this);
         homePagePresenter.takeView(this);
 
-        lostCardAdapter = new LostCardAdapter(MainActivity.this, R.layout.lostcard_item, mLostCards);
-        listView.setAdapter(lostCardAdapter);
 
-        homePagePresenter.getPageOfLosts(pageNumber);
-
-        listView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener2<ListView>() {
-            @Override
-            public void onPullDownToRefresh(PullToRefreshBase<ListView> refreshView) {
-                Toast.makeText(MainActivity.this,"pulldownrefresh",Toast.LENGTH_SHORT).show();
-                refreshView.onRefreshComplete();
-            }
-
-            @Override
-            public void onPullUpToRefresh(PullToRefreshBase<ListView> refreshView) {
-                homePagePresenter.getPageOfLosts(++pageNumber);
-                refreshView.onRefreshComplete();
-            }
-        });
-
+        //        initLostCards();
 
 
         //TODO:方便使用，到时候去掉
@@ -124,10 +94,9 @@ public class MainActivity extends AppCompatActivity implements IHomePageView {
 
     @Override
     public void refreshView(List<LostCard> lostCards) {
-
-        mLostCards.addAll(lostCards);
-        lostCardAdapter.notifyDataSetChanged();
-
+        LostCardAdapter lostCardAdapter = new LostCardAdapter(MainActivity.this, R.layout.lostcard_item, lostCards);
+        PullToRefreshListView listView = (PullToRefreshListView) findViewById(R.id.listview_lostcards);
+        listView.setAdapter(lostCardAdapter);
     }
 
     @Override
@@ -140,6 +109,7 @@ public class MainActivity extends AppCompatActivity implements IHomePageView {
     @Override
     protected void onResume(){
         super.onResume();
+        homePagePresenter.getPageOfLosts(1);
     }
 
     @Override
