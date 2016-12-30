05848d3156a8163e6ec9a4f6c38eafaf31778fc5
runnishino <237485393@qq.com>
2016-12-31 00:48:36 +0800

    添加lostDetail界面，为编写界面逻辑代码

---
+++ Flosing/app/src/main/java/com/our/flosing/view/LostDetailActivity.java
@@ -0,0 +1,21 @@
+package com.our.flosing.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.our.flosing.R;
+
+/**
+ * Created by RunNishino on 2016/12/31.
+ */
+
+public class LostDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_lost_detail);
+
+
+    }
+}
