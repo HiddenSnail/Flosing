c5a48335e5f4891659aa9ff645958aa1a6550d43
runnishino <237485393@qq.com>
2017-01-03 21:15:51 +0800

    准备获取界面部分

--- Flosing/app/src/main/java/com/our/flosing/view/MainActivity.java
+++ Flosing/app/src/main/java/com/our/flosing/view/MainActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 import com.avos.avoscloud.AVUser;
@@ -29,7 +31,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+
+//        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
 
         lost_list = (Button) findViewById(R.id.lost_list);
